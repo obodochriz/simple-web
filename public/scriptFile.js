5f10950acbe3a0d2a419d7899df3376b7707dde2
unknown <levin00701@gmail.com>
2023-08-29 16:25:23 +0700

    v1

---
+++ public/scriptFile.js
@@ -0,0 +1,6 @@
+let h1Tag = document.getElementById('h1');
+let buttonTag = document.getElementById('btn');
+
+buttonTag.addEventListener('click', function(){
+    h1Tag.innerHTML = "Welcome to this course";
+})
